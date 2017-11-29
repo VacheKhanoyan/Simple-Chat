f78e66c4e27493cb781922edb75a5ab8355b907b
VacheKhanoyan <khanoyann@gmail.com>
2017-11-29 19:56:34 +0400

    chnage file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var WebSocketServer = require('ws').Server;
+let WebSocketServer = require('ws').Server;
 
-var wss = new WebSocketServer({port: 1984});
+let wss = new WebSocketServer({port: 1984});
 
-var messages = [];
+let messages = [];
 wss.on('connection', function (ws) {
   messages.forEach(function(message){
     ws.send(message);
